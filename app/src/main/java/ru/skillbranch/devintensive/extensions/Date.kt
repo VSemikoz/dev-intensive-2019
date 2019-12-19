a4eaa1c13102c56597fd9ae8aff3e27feda18bbe
VSemikoz <slava.semik@yandex.ru>
2019-12-19 22:51:04 +0300

    date plural

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -32,6 +32,36 @@ fun Date.humanizeDiff(date: Date = Date()): String {
     TODO("not implemented")
 }
 
+fun TimeUnits.plural(countOfUnits: Int): String {
+    val n = Math.abs(countOfUnits) % 100
+    val n1 = n % 10
+    return "$countOfUnits " + when(this){
+        TimeUnits.SECOND -> when{
+            n in 11..19 -> "секунд"
+            n1 in 2..4 -> "секунды"
+            n1 == 1 -> "секунду"
+            else -> "секунд"
+        }
+        TimeUnits.MINUTE -> when{
+            n in 11..19 -> "минут"
+            n1 in 2..4 -> "минуты"
+            n1 == 1 -> "минуту"
+            else -> "минут"
+        }
+        TimeUnits.HOUR -> when{
+            n in 11..19 -> "часов"
+            n1 in 2..4 -> "часа"
+            n1 == 1 -> "час"
+            else -> "часов"
+        }
+        TimeUnits.DAY -> when{
+            n in 11..19 -> "дней"
+            n1 in 2..4 -> "дня"
+            n1 == 1 -> "день"
+            else -> "дней"
+        }
+    }
+}
 enum class TimeUnits {
     SECOND,
     MINUTE,

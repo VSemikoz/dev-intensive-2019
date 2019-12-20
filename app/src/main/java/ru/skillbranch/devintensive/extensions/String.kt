62bfdc440c79f487aefdbc32fef14535060f2f60
VSemikoz <slava.semik@yandex.ru>
2019-12-20 03:00:33 +0300

    String extentios file

---
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/String.kt
@@ -0,0 +1,22 @@
+package ru.skillbranch.devintensive.extensions
+
+fun String.truncate(maxSymbols: Int = 16): String{
+    val trimString = this.trim()
+    if (trimString.length > maxSymbols){
+        return "${trimString.slice(0 until maxSymbols).trim()}..."
+    }
+    return trimString
+}
+
+fun String.stripHtml(): String{
+    val index1 = this.indexOfFirst { it == '>'}
+    val index2 = this.indexOfLast { it == '<'}
+    var subString = this.substring(index1 + 1, index2)
+
+    while(subString.contains("  ")) {
+        var replace = subString.replace("  ", " ")
+        subString = replace
+    }
+    return subString
+
+}
--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -3,35 +3,12 @@ package ru.skillbranch.devintensive.utils
 import android.annotation.SuppressLint
 
 val TRANSLITERATION_DICT = mapOf(
-    "а" to "a", "б" to "b", "в" to "v", "г" to "g",
-    "д" to "d", "е" to "e", "ё" to "e", "ж" to "zh", "з" to "z",
-    "и" to "i", "й" to "i", "к" to "k", "л" to "l", "м" to "m",
-    "н" to "n", "о" to "o", "п" to "p", "р" to "r", "с" to "s",
-    "т" to "t", "у" to "u", "ф" to "f", "х" to "h", "ц" to "c",
-    "ч" to "ch", "ш" to "sh", "щ" to "sh", "ъ" to "", "ы" to "i",
-    "ь" to "", "э" to "e", "ю" to "yu", "я" to "ya"
+    "а" to "a", "б" to "b", "в" to "v", "г" to "g", "д" to "d", "е" to "e", "ё" to "e", "ж" to "zh",
+    "з" to "z", "и" to "i", "й" to "i", "к" to "k", "л" to "l", "м" to "m", "н" to "n", "о" to "o",
+    "п" to "p", "р" to "r", "с" to "s", "т" to "t", "у" to "u", "ф" to "f", "х" to "h", "ц" to "c",
+    "ч" to "ch", "ш" to "sh", "щ" to "sh", "ъ" to "", "ы" to "i", "ь" to "", "э" to "e", "ю" to "yu",
+    "я" to "ya"
 )
-
-fun String.truncate(maxSymbols: Int = 16): String{
-    val trimString = this.trim()
-    if (trimString.length > maxSymbols){
-        return "${trimString.slice(0 until maxSymbols).trim()}..."
-    }
-    return trimString
-}
-
-fun String.stripHtml(): String{
-    val index1 = this.indexOfFirst { it == '>'}
-    val index2 = this.indexOfLast { it == '<'}
-    var subString = this.substring(index1 + 1, index2)
-
-    while(subString.contains("  ")) {
-        var replace = subString.replace("  ", " ")
-        subString = replace
-    }
-    return subString
-
-}
 
 object Utils {
 
